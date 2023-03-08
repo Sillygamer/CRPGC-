6030d5b6b56f7bb43a571f2d0508c666b250c012
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2023-03-08 13:35:17 -0800

    world being implemented

--- New folder/CRPG/World.cpp
+++ New folder/CRPG/World.cpp
@@ -74,6 +74,44 @@ void World::PopulateLocations()
 	locations.push_back(LicoriceForest);
 }
 
+void World::PopulateMonsters()
+{
+	/*
+	Monster rat = new Monster(MONSTER_ID_RAT, "Rat", 5, 3, 10, 3, 3);
+	rat.LootTable.Add(new LootItem(ItemByID(ITEM_ID_RAT_TAIL), 75, false));
+	rat.LootTable.Add(new LootItem(ItemByID(ITEM_ID_PIECE_OF_FUR), 75, true));
+
+	Monster snake = new Monster(MONSTER_ID_SNAKE, "Snake", 5, 3, 10, 3, 3);
+	snake.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SNAKE_FANG), 75, false));
+	snake.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SNAKESKIN), 75, true));
+	snake.LootTable.Add(new LootItem(ItemByID(ITEM_ID_CLUB), 75, true));
+
+	Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "Giant spider", 20, 5, 40, 10, 10);
+	giantSpider.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SPIDER_FANG), 75, true));
+	giantSpider.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SPIDER_SILK), 25, false));
+
+	Monsters.push_backd(rat);
+	Monsters.push_back(snake);
+	Monsters.push_back(giantSpider);
+	*/
+}
+
+void World::PopulateItems()
+{
+
+	//Items.push_back(new Weapons(ITEM_ID_RUSTY_SWORD, "Rusty sword", "Rusty swords", 0, 5));
+	//Items.push_back(new Item(ITEM_ID_RAT_TAIL, "Rat tail", "Rat tails"));
+	//Items.push_back(new Item(ITEM_ID_PIECE_OF_FUR, "Piece of fur", "Pieces of fur"));
+	//Items.push_back(new Item(ITEM_ID_SNAKE_FANG, "Snake fang", "Snake fangs"));
+	//Items.push_back(new Item(ITEM_ID_SNAKESKIN, "Snakeskin", "Snakeskins"));
+	//Items.push_back(new Weapons(ITEM_ID_CLUB, "Club", "Clubs", 3, 10));
+	//Items.push_back(new HealingPotion(ITEM_ID_HEALING_POTION, "Healing potion", "Healing potions", 5));
+	//Items.push_back(new Item(ITEM_ID_SPIDER_FANG, "Spider fang", "Spider fangs"));
+	//Items.push_back(new Item(ITEM_ID_SPIDER_SILK, "Spider silk", "Spider silks"));
+	//Items.push_back(new Item(ITEM_ID_ADVENTURER_PASS, "Adventurer pass", "Adventurer passes"));
+
+}
+
 void World::ListLocations()
 {
 	cout << " These are the Locations in the world...\n\n " 
@@ -98,4 +136,65 @@ Location World::Locationbyid(int id)
 
 }
 
+
+
+//need to change
+
+
+
+
+//need to change
+
+private static void PopulateQuests()
+{
+	Quest clearAlchemistGarden =
+		new Quest(
+			QUEST_ID_CLEAR_ALCHEMIST_GARDEN,
+			"Clear the alchemist's garden",
+			"Kill rats in the alchemist's garden and bring back 3 rat tails. \nYou will receive a healing potion and 10 gold pieces.", 20, 10);
+
+	clearAlchemistGarden.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_RAT_TAIL), 3));
+
+	clearAlchemistGarden.RewardItem = ItemByID(ITEM_ID_HEALING_POTION);
+
+	Quest clearFarmersField =
+		new Quest(
+			QUEST_ID_CLEAR_FARMERS_FIELD,
+			"Clear the farmer's field",
+			"Kill snakes in the farmer's field and bring back 3 snake fangs. \nYou will receive an adventurer's pass and 20 gold pieces.", 20, 20);
+
+	clearFarmersField.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_SNAKE_FANG), 3));
+
+	clearFarmersField.RewardItem = ItemByID(ITEM_ID_ADVENTURER_PASS);
+
+	Quests.Add(clearAlchemistGarden);
+	Quests.Add(clearFarmersField);
+}
+
+public static Monster MonsterByID(int id)
+public static Monster MonsterByID(int id)
+{
+	for (Monster monster : Monsters)
+	{
+		if (monster.ID == id)
+		{
+			return monster;
+		}
+	}
+
+	return null;
+}
+public static Quest QuestByID(int id)
+public static Quest QuestByID(int id)
+{
+	for (Quest quest in Quests)
+	{
+		if (quest.ID == id)
+		{
+			return quest;
+		}
+	}
+
+	return null;
+}
 
--- New folder/CRPG/World.h
+++ New folder/CRPG/World.h
@@ -4,6 +4,9 @@
 #include <list>
 #include <algorithm>
 #include <vector>
+#include "Weapons.h"
+#include "HealingPotion.h"
+#include "Item.h"
 using namespace std;
 
 class World
@@ -18,6 +21,28 @@ public:
 	World();
 	virtual ~World();
 	void PopulateLocations();
+	vector<Item> Items;
+	vector<Monster> Monsters;
+	vector<Quest> Quests;
+	void PopulateMonsters();
+	const int ITEM_ID_RUSTY_SWORD = 1;
+	const int ITEM_ID_RAT_TAIL = 2;
+	const int ITEM_ID_PIECE_OF_FUR = 3;
+	const int ITEM_ID_SNAKE_FANG = 4;
+	const int ITEM_ID_SNAKESKIN = 5;
+	const int ITEM_ID_CLUB = 6;
+	const int ITEM_ID_HEALING_POTION = 7;
+	const int ITEM_ID_SPIDER_FANG = 8;
+	const int ITEM_ID_SPIDER_SILK = 9;
+	const int ITEM_ID_ADVENTURER_PASS = 10;
+    void PopulateQuests()
+	const int MONSTER_ID_RAT = 1;
+	const int MONSTER_ID_SNAKE = 2;
+	const int MONSTER_ID_GIANT_SPIDER = 3;
+
+	const int QUEST_ID_CLEAR_ALCHEMIST_GARDEN = 1;
+	const int QUEST_ID_CLEAR_FARMERS_FIELD = 2;
+	void PopulateItems();
 
 	int LOCATION_ID_GINGERBREAD_HOUSE = 1;
 	int LOCATION_ID_CANDYCANE_FOREST = 2;
